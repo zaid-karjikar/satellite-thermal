2b6a44e3f70bf4e66e5688c709065d5c421de944
Zaid Karjikar <itszaidkarjikar@gmail.com>
2026-04-13 04:09:56 +0200

    chore: add constants.py

---
+++ src/constants.py
@@ -0,0 +1,5 @@
+# constants.py
+
+# Earth
+EARTH_RADIUS_KM: float = 6378.137              # WGS-84 equatorial radius 
+EARTH_MU_KM3_S2: float = 398600.4418           # Standard gravitational parameter
